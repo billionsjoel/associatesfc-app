764225e5583b4206797849185cef517f0df94cba
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2025-04-27 12:39:45 +0300

    Create cards for match

--- app/LandingPage/Components/Year.tsx
+++ app/LandingPage/Components/Year.tsx
@@ -1,11 +1,18 @@
 import styles from './css/yearSection.module.css';
+import MatchCard from './Match-card';
 
 export default function YearSection() {
 	return (
 		<>
-			<div className={`${styles.yearContainer}`}>
-				<hr className={`offset-1 ${styles.hr}`} />
-				<h4 className="offset-1">This Year At Associates.</h4>
+			<div className={`bg-dark ${styles.yearContainer}`}>
+				<hr className={`bg-danger offset-1 ${styles.hr}`} />
+				<h4 className="offset-1 text-white">THIS YEAR AT ASSOCIATES</h4>
+				<div className="row gap-4 mt-4 offset-md-1">
+					<MatchCard></MatchCard>
+					<MatchCard></MatchCard>
+					<MatchCard></MatchCard>
+					<MatchCard></MatchCard>
+				</div>
 			</div>
 		</>
 	);
