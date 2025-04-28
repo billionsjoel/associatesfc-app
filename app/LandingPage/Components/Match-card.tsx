13ac0eb90ff32d3f6e28fab870f3ab735a2557c7
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2025-04-28 23:19:19 +0300

    Update year fixture.

--- app/LandingPage/Components/Match-card.tsx
+++ app/LandingPage/Components/Match-card.tsx
@@ -7,25 +7,25 @@ export default function MatchCard() {
 		<>
 			<div className={`card ${matchStyles.matchCard} d-flex flex-row align-items-center`}>
 				<div className="flex-grow-1">
-					<small className="text-white">Today</small>
-					<h6 className={`${matchStyles.cardTitle} mb-1 mt-2 fw-light`}>English Premier League 2</h6>
+					<small className="text-white">11-07-2025</small>
+					<h6 className={`${matchStyles.cardTitle} mb-1 mt-2 fw-light`}>Masaka</h6>
 
 					<div className="d-flex align-items-center mb-2 mt-2">
 						<Image src="/images/logo.jpeg" alt="Team logo" width={30} height={10} className={`${matchStyles.teamLogo} me-2 ${styles.logo}`} />
-						<span>Man Utd U21</span>
+						<span>Associates fc</span>
 					</div>
 
 					<div className="d-flex align-items-center">
-						<Image src="/images/logo.jpeg" alt="Team logo" width={30} height={10} className={`${matchStyles.teamLogo} me-2 ${styles.logo}`} />
-						<span>Everton U21</span>
+						<Image src="https://img.logo.dev/manutd.com" alt="Team logo" width={30} height={10} className={`${matchStyles.teamLogo} me-2 ${styles.logo}`} />
+						<span>Masaka fc</span>
 					</div>
 				</div>
 
 				<div className="vr mx-3"></div>
 
 				<div className="text-end">
-					<div className="fw-light">Kick Off - 21:00</div>
-					<small className="text-muted">Leigh Sports Village</small>
+					<div className="fw-light">Kick Off - 2:00</div>
+					<small className="text-muted">Masaka Sports Village</small>
 
 					<div className="mt-3">
 						<a href="#" className={`${matchStyles.cardBtn} btn btn-light btn-sm fw-light`}>
