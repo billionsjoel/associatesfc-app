eaad622ee128ba9cabccf879c61aa9a8d56396e7
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2025-04-28 07:30:38 +0300

    Update Year.tsx

--- app/LandingPage/Components/Year.tsx
+++ app/LandingPage/Components/Year.tsx
@@ -5,8 +5,8 @@ export default function YearSection() {
 	return (
 		<>
 			<div className={`bg-dark ${styles.yearContainer}`}>
-				<hr className={`bg-danger offset-1 ${styles.hr}`} />
-				<h4 className="offset-1 text-white">THIS YEAR AT ASSOCIATES</h4>
+				<hr className={`bg-danger offset-md-1 ${styles.hr}`} />
+				<h4 className="offset-md-1 text-white">THIS YEAR AT ASSOCIATES</h4>
 				<div className="row gap-4 mt-4 offset-md-1">
 					<MatchCard></MatchCard>
 					<MatchCard></MatchCard>
